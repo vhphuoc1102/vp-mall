8d66f8d9f02ee8796b9820e3c1c07cb032206c61
Phuoc Hong Vu <phuocvu511@gmail.com>
2023-11-25 18:06:20 +0700

    Create discovery, config server

---
+++ vp-gateway/src/main/java/com/phuocvh/vpgateway/VpGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.phuocvh.vpgateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpGatewayApplication {
+    public static void main(String[] args) {
+
+    }
+}
