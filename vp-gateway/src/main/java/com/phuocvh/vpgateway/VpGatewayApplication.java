5b35b386f9011dfb34407649596221e35c0219c8
Phuoc Hong Vu <phuocvu511@gmail.com>
2024-01-02 16:59:13 +0700

    Add feature: API gateway

--- vp-gateway/src/main/java/com/phuocvh/vpgateway/VpGatewayApplication.java
+++ vp-gateway/src/main/java/com/phuocvh/vpgateway/VpGatewayApplication.java
@@ -3,6 +3,7 @@ package com.phuocvh.vpgateway;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class VpGatewayApplication {
     public static void main(String[] args) {
 
